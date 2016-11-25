45a156d8137021b1e0965ac77cab8b28da4bbb4d
christianbegines <christianbeginescarrasco@gmail.com>
2016-11-25 22:13:49 +0100

    git init, feat:listado de obejetos Angular 2

---
+++ app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  { id: 0, name: 'Illidan'}
+  { id: 1, name: 'Thrall' },
+  { id: 2, name: 'Varyan' },
+  { id: 3, name: 'Shylvanas' },
+  { id: 4, name: 'Vol´jin' },
+  { id: 5, name: 'Anduyn' },
+  { id: 6, name: 'Cringris' },
+  { id: 7, name: 'Vel´len' },
+  { id: 8, name: 'Arthas' },
+  { id: 9, name: 'Ogrim' },
+  { id: 10, name: 'Durotan' }
+];
