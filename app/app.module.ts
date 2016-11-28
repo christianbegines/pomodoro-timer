637bec7cf02b774f94a897c1b143e78d640e9555
Alberto Acevedo <enrique.nunyez@gnommostudios.com>
2016-11-28 19:56:51 +0100

    pusheito bueno

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,31 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { AppComponent }  from './app.component';
-import { HeroDetalleComponent } from './hero-detalle.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroServicio } from './hero.servicio';
-import { DashboardComponent } from './dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
-
-
+import { PomodoroTimer }  from './pomodoro-timer.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
    ],
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetalleComponent,
-    HeroesComponent
+    PomodoroTimer
   ],
-  providers:[
-    HeroServicio
-  ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ PomodoroTimer ]
 })
 export class AppModule { }
---
+++ app/pomodoro-timer.component.ts
@@ -0,0 +1,51 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'pomodoro-timer',
+  template: `
+  <h1>{{minutes}}:{{seconds | number:'2.0'}}</h1>
+  <p>
+    <button (click)="togglePause()">
+        {{buttonLabel}}
+    </button>  
+  </p>
+  `
+})
+export class PomodoroTimer  {
+  minutes:number;
+  seconds:number;
+  isPaused:boolean;
+  buttonLabel:string;
+
+  constructor(){
+    this.resetPomodoro();
+    setInterval(() => this.tick(),1000);
+  }
+
+  private tick():void{
+    if(!this.isPaused) {
+      this.buttonLabel = 'Pause';
+
+      if (--this.seconds < 0) {
+        this.seconds = 59;
+        if (--this.minutes < 0) {
+          this.resetPomodoro();
+        }
+      }
+    }
+  }
+  resetPomodoro():void{
+    this.minutes=24;
+    this.seconds=59;
+    this.buttonLabel='Start';
+    this.togglePause();
+  }
+
+  togglePause():void{
+    this.isPaused = !this.isPaused;
+    if(this.minutes<24 || this.seconds<59){
+      this.buttonLabel = this.isPaused ? 'Resume':'Pause';
+    }
+  }
+
+}
